e886ea375bffb6c9de4861ed40cb81fe682af86e
Tenzin <tenthinten@gmail.com>
2022-02-06 19:44:55 +0530

    add image and model name

--- src/components/Section.js
+++ src/components/Section.js
@@ -4,7 +4,10 @@ import styled from 'styled-components';
 function Section() {
     return (
         <Wrap>
-            
+            <ItemText>
+                <h1>Model S</h1>
+                <p>Order Online for Touchless Delivery</p>
+            </ItemText>
         </Wrap>
     )
 }
@@ -14,5 +17,12 @@ export default Section;
 const Wrap = styled.div`
     width: 100vw;
     height: 100vh;
-    background: orange;
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-image: url('/images/model-s.jpg')
+`
+const ItemText = styled.div`
+    padding-top: 15vh;
+    text-align: center;
 `
