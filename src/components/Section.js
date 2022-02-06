bf16f7fd6efbe65673278dc3a7447b3df950eb3e
Tenzin <tenthinten@gmail.com>
2022-02-06 20:07:57 +0530

    buttons for section

--- src/components/Section.js
+++ src/components/Section.js
@@ -8,6 +8,15 @@ function Section() {
                 <h1>Model S</h1>
                 <p>Order Online for Touchless Delivery</p>
             </ItemText>
+            <ButtonGroup>
+                <LeftButton>
+                    Custom Order
+                </LeftButton>
+                <RightButton>
+                    Existing Inventory
+                </RightButton>
+            </ButtonGroup>
+            <DownArrow src="/images/down-arrow.svg"/>
         </Wrap>
     )
 }
@@ -20,9 +29,39 @@ const Wrap = styled.div`
     background-size: cover;
     background-position: center;
     background-repeat: no-repeat;
-    background-image: url('/images/model-s.jpg')
+    background-image: url('/images/model-s.jpg');
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between; //vertical
+    align-items: center; //horizotal
 `
 const ItemText = styled.div`
     padding-top: 15vh;
     text-align: center;
+`
+const ButtonGroup = styled.div`
+    display:flex;
+    margin-bottom: 30px;
+`
+const LeftButton = styled.div`
+    background-color: rgba(23, 26, 32, 0.8);
+    height:40px;
+    width: 256px;
+    color:white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 100px;
+    opacity: 0.85;
+    text-transform: uppercase;
+    font-size: 12px;
+    cursor:pointer;
+`
+const RightButton = styled(LeftButton)`
+
+`
+
+const DownArrow = styled.img`
+    margin-top: 20px;
+    height: 40px;
 `
