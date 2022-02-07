9ba4188492b7ba646ebf7e9260d40ab18f4b283e
Tenzin <tenthinten@gmail.com>
2022-02-07 11:39:58 +0530

    using props to add multiple sections

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import styled from 'styled-components';
 
-function Section() {
+function Section({title,description,leftBtnText,rightBtnText,backgroundImg}) {
     return (
-        <Wrap>
+        <Wrap bgImage={backgroundImg}> 
             <ItemText>
-                <h1>Model S</h1>
-                <p>Order Online for Touchless Delivery</p>
+                <h1>{title}</h1>
+                <p>{description}</p>
             </ItemText>
             <Buttons>
             <ButtonGroup>
                 <LeftButton>
-                    Custom Order
+                    {leftBtnText}
                 </LeftButton>
                 <RightButton>
-                    Existing Inventory
+                    {rightBtnText}
                 </RightButton>
             </ButtonGroup>
             <DownArrow src="/images/down-arrow.svg"/>
@@ -37,6 +37,7 @@ const Wrap = styled.div`
     flex-direction: column;
     justify-content: space-between; //vertical
     align-items: center; //horizotal
+    background-image: ${props => `url("/images/${props.bgImage}")`}
 `
 const ItemText = styled.div`
     padding-top: 15vh;
@@ -45,6 +46,9 @@ const ItemText = styled.div`
 const ButtonGroup = styled.div`
     display:flex;
     margin-bottom: 30px;
+    @media (max-width: 768px) {
+        flex-direction: column;
+    }
 `
 const LeftButton = styled.div`
     background-color: rgba(23, 26, 32, 0.8);
@@ -62,11 +66,12 @@ const LeftButton = styled.div`
     margin: 8px;
 `
 const RightButton = styled(LeftButton)`
-
+    background: white;
+    opacity: 0.65;
+    color: black;
 `
 
 const DownArrow = styled.img`
-    margin-top: 20px;
     height: 40px;
     overflow-x: hidden;
     animation: animateDown infinite 1.5s;
