6f9af52cec55409366a2ff72c3acfdc0b8b1ce99
Tenzin <tenthinten@gmail.com>
2022-02-07 12:00:27 +0530

    add tesla logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,33 @@
 import React from 'react';
+import styled from 'styled-components';
 
 function Header() {
     return (
-        <div>
-            Header
-        </div>
+        <Container>
+            <a>
+                <img src="/images/logo.svg" alt="" />
+            </a>
+            <Menu>
+                <p><a href="#">Model S</a></p>
+                <p><a href="#">Model 3</a></p>
+                <p><a href="#">Model x</a></p>
+                <p><a href="#">Model Y</a></p>
+            </Menu>
+        </Container>
     )
 }
 
 export default Header;
+
+
+const Container = styled.div`
+    min-height: 60px;
+    position: fixed;
+    display: flex;
+    align-items:center;
+    padding: 0 20px;
+`
+
+const Menu = styled.div`
+
+`
